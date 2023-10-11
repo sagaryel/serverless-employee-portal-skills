5026435a1e5f8e1a582498fe9737039c2c983e20
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2023-10-11 18:21:14 +0530

    new  commit

--- app.js
+++ app.js
@@ -63,7 +63,6 @@ module.exports.getEmployeeSkill = async (event) => {
             try {
                 const input = {
                     TableName: process.env.DYNAMODB_TABLE_NAME,
-                    TableName: process.env.DYNAMODB_TABLE_NAME,
                     ProjectionExpression: " skilName, yearsOfKnowledge, skillLevel, certified, isActive, createdDateTime, updatedDateTime",
                     FilterExpression: "IsActive = :isActive",        // Add a FilterExpression if you want to filter the results
                     ExpressionAttributeValues: {
