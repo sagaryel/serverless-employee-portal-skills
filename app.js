9bb84c1f63e0fcfed8d53e047dd6153fa351c3ed
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2023-10-12 18:28:11 +0530

    latest  commit

--- app.js
+++ app.js
@@ -65,10 +65,10 @@ module.exports.getEmployeeSkill = async (event) => {
                 const input = {
                     TableName: process.env.DYNAMODB_TABLE_NAME,
                     ProjectionExpression: " skilName, yearsOfKnowledge, skillLevel, certified, isActive, createdDateTime, updatedDateTime",
-                    // FilterExpression: "IsActive = :isActive",        // Add a FilterExpression if you want to filter the results
-                    // ExpressionAttributeValues: {
-                    //     ":isActive": true
-                    // }
+                     FilterExpression: "isActive = :isActive",        // Add a FilterExpression if you want to filter the results
+                     ExpressionAttributeValues: {
+                         ":isActive": true
+                     }
                 };
                 //Await response from db when sent scan command with tablename
                 const { Items } = await db.send(new ScanCommand(input));
