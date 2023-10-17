1cd94f568fb10be98a9e680b214a9d402ba5bfe1
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2023-10-17 11:48:04 +0530

    first  commit

--- app.js
+++ app.js
@@ -64,11 +64,11 @@ module.exports.getEmployeeSkill = async (event) => {
             try {
                 const input = {
                     TableName: process.env.DYNAMODB_TABLE_NAME,
-                    ProjectionExpression: " empId, skilName, yearsOfKnowledge, skillLevel, certified, isActive, createdDateTime, updatedDateTime",
-                    //   FilterExpression: "softDelete = :softDeleteCondition",        // Add a FilterExpression if you want to filter the results
-                    //   ExpressionAttributeValues: {
-                    //       ":softDeleteCondition": false
-                    //   }
+                    ProjectionExpression: "empId, skilName, yearsOfKnowledge, skillLevel, certified, isActive, createdDateTime, updatedDateTime",
+                    FilterExpression: "softDelete = :softDeleteCondition",        // Add a FilterExpression if you want to filter the results
+                       ExpressionAttributeValues: {
+                           ":softDeleteCondition": false
+                       }
                 };
                 //Await response from db when sent scan command with tablename
                 const { Items } = await db.send(new ScanCommand(input));
